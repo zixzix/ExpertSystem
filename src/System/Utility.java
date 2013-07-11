f2772f6c3ec166cf5e152c9a8c4b234bf1114bd5
Pasquale <Pasquale@Prolog>
2013-07-11 11:40:21 +0200

    Prova

---
+++ src/System/Utility.java
@@ -0,0 +1,32 @@
+package System;
+
+import java.util.Hashtable;
+
+import jpl.Atom;
+import jpl.Query;
+import jpl.Term;
+import jpl.Variable;
+
+public class Utility {
+    public static void main(String[] args) {
+        System.out.println("Sei un coglione!!! Pasquale và con i neri sordi..."); // Display the string.
+        System.out.println("Ricordiamoci di mantenere un contegno perchè il progetto è pubblico"); // Display the string.
+        
+        Query q1 = new Query("consult", new Term[] {new Atom("prolog.pl")} );
+        q1.open();
+        q1.getSolution();
+        
+        
+        Query q2 = new Query("descendent_of",new Term[] {new Variable("X"),new Atom("joe")} );
+    	
+    	Hashtable solution;
+    	
+
+    	while ( q2.hasMoreSolutions() ){
+    	    solution = q2.nextSolution();
+    	    System.out.println( "X = " + solution.get("X"));
+    	}
+        
+    }
+
+}
